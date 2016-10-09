cd9348d67824c1e65825a537c3b562c823bb1d18
wolfram74 <phaugen@gmail.com>
2016-10-08 19:19:43 -0500

    how bout that?

--- project/home/views.py
+++ project/home/views.py
@@ -1,7 +1,15 @@
 from flask import render_template, Blueprint, flash, url_for
+from markovbot import MarkovBot
+import os
+import sys
+
 
 home_blueprint = Blueprint('home', __name__, template_folder='templates')
 
 @home_blueprint.route('/', methods=['GET'])
 def home():
-    return render_template('index.html')
+    print(sys.argv[0])
+    bot = MarkovBot()
+    bot.read('./project/home/assets/hank_log.txt')
+    string = bot.generate_text(25)
+    return render_template('index.html', filler=string)
