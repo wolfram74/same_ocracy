808ecd4d8adca83384813f7f753ce0e03b6ea3d9
wolfram74 <phaugen@gmail.com>
2016-10-08 21:55:01 -0500

    basic route set up

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # same_ocracy
+bot_quality
+  bot_id: int
+  guesses: int
+  correct: int
+
+markov bot occasionally fails
--- project/home/views.py
+++ project/home/views.py
@@ -2,14 +2,25 @@ from flask import render_template, Blueprint, flash, url_for
 from markovbot import MarkovBot
 import os
 import sys
-
+import random
 
 home_blueprint = Blueprint('home', __name__, template_folder='templates')
 
 @home_blueprint.route('/', methods=['GET'])
 def home():
-    print(sys.argv[0])
+    fork = random.randint(0,2)
+    if fork==0:
+        # identify actual tweet
+        pass
+    elif fork ==1:
+        # identify fake tweet
+        pass
+    elif fork==2:
+        # compare two twitter bots
+        pass
+
     bot = MarkovBot()
-    bot.read('./project/home/assets/hank_log.txt')
+    bot.read('./project/assets/hank_log.txt')
     string = bot.generate_text(25)
-    return render_template('index.html', filler=string)
+    subject = 'Hank Green'
+    return render_template('index.html', tweet=string, left=subject, right=subject+ ' twitter bot')
